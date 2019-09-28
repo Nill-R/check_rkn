6c870a356ad458e1fb02f2c6f02d64f8d239f9ce
Nill-R <47075922+Nill-R@users.noreply.github.com>
2019-09-29 01:44:36 +0200

    Remove blank string

--- check_rkn_ips.bash
+++ check_rkn_ips.bash
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 IP=`curl -s -X POST -d '["198.51.100.1", "198.51.100.43"]' http://localhost:9999/check_ips|jq . |grep true|awk '{ print $1 }'|tr -d '"'|tr -d ':'`
 if [ -n "$IP" ]; then
