c6a0b9a1031011cd2c019846a9d79cbe1696df30
Nill-R <47075922+Nill-R@users.noreply.github.com>
2019-09-29 17:23:59 +0200

    Move IPs to variables

--- check_rkn_ips.bash
+++ check_rkn_ips.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+IP1='"104.31.93.48", "104.31.92.48"'
+IP2='"18.130.154.47"'
 HOST=`hostname`
 DATE=`date`
-IP=`curl -s -X POST -d '["104.31.93.48", "104.31.92.48" ]' http://localhost:9999/check_ips|jq . |grep true|awk '{ print $1 }'|tr -d '"'|tr -d ':'`
+IP=`curl -s -X POST -d "[$IP1,$IP2]" http://localhost:9999/check_ips|jq . |grep true|awk '{ print $1 }'|tr -d '"'|tr -d ':'`
 
 if [ -n "$IP" ]; then
         printf "$IP\n Is in RKN blacklist\n Sending by telegram-send from host $HOST \n $DATE" |telegram-send --stdin
