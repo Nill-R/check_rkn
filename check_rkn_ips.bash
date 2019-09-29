3fd3752f780e1d9f822d3ab55b4118016261d583
Nill-R <47075922+Nill-R@users.noreply.github.com>
2019-09-29 16:38:17 +0200

    Update check_rkn_ips.bash

--- check_rkn_ips.bash
+++ check_rkn_ips.bash
@@ -1,7 +1,11 @@
 #!/bin/bash
-IP=`curl -s -X POST -d '["198.51.100.1", "198.51.100.43"]' http://localhost:9999/check_ips|jq . |grep true|awk '{ print $1 }'|tr -d '"'|tr -d ':'`
+
+HOST=`hostname`
+DATE=`date`
+IP=`curl -s -X POST -d '["104.31.93.48", "104.31.92.48" ]' http://localhost:9999/check_ips|jq . |grep true|awk '{ print $1 }'|tr -d '"'|tr -d ':'`
+
 if [ -n "$IP" ]; then
-        telegram-send "$IP in RKN blacklist"
+        printf "$IP\n Is in RKN blacklist\n Sending by telegram-send from host $HOST \n $DATE" |telegram-send --stdin
    else
         exit 0
 fi
