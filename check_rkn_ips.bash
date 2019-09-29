f3c2518fb66256d73023f5aaa6aaf33786e1817c
Nill Rinov <nill.rinov@pm.me>
2019-09-29 18:11:15 +0000

    Formating main.go and check_rkn_ips.bash

--- check_rkn_ips.bash
+++ check_rkn_ips.bash
@@ -2,12 +2,12 @@
 
 IP1='"104.31.93.48", "104.31.92.48"'
 IP2='"18.130.154.47"'
-HOST=`hostname`
-DATE=`date`
-IP=`curl -s -X POST -d "[$IP1,$IP2]" http://localhost:9999/check_ips|jq . |grep true|awk '{ print $1 }'|tr -d '"'|tr -d ':'`
+HOST=$(hostname)
+DATE=$(date)
+IP=$(curl -s -X POST -d "[$IP1,$IP2]" http://localhost:9999/check_ips | jq . | grep true | awk '{ print $1 }' | tr -d '"' | tr -d ':')
 
 if [ -n "$IP" ]; then
-        printf "$IP\n Is in RKN blacklist\n Sending by telegram-send from host $HOST \n $DATE" |telegram-send --stdin
-   else
-        exit 0
+	printf "$IP\n Is in RKN blacklist\n Sending by telegram-send from host $HOST \n $DATE" | telegram-send --stdin
+else
+	exit 0
 fi
